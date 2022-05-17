60a313147d1f4012f95247494429d6d72e7e2cac
RetaxMaster <retaxmaster@hotmail.com>
2022-05-17 17:16:10 -0500

    24 chat

--- src/realtimeServer.js
+++ src/realtimeServer.js
@@ -5,7 +5,14 @@ module.exports = httpServer => {
 
     io.on("connection", socket => {
 
-        console.log(socket.id);
+       socket.on("message", message => {
+
+        io.emit("message", {
+            user: "Retaxito",
+            message
+        });
+
+       })
 
     });
 
